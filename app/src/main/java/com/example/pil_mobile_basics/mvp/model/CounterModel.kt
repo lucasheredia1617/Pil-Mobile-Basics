aef8bbf6d54e3b56ac8f5a62edd84911e6de4fda
Lucas Heredia <lucasheredia5475@gmail.com>
2023-04-02 02:16:41 -0300

    model

--- app/src/main/java/com/example/pil_mobile_basics/mvp/model/CounterModel.kt
+++ app/src/main/java/com/example/pil_mobile_basics/mvp/model/CounterModel.kt
@@ -1,4 +1,30 @@
-package com.example.pil_mobile_basics.mvp.model
+package com.example.pil_mobile_basics.mvp.model // ktlint-disable package-name
 
-class CounterModel {
+import com.example.pil_mobile_basics.mvp.contract.CounterContract
+import com.example.pil_mobile_basics.util.Contants.ZERO
+
+// ktlint-disable package-name
+
+class CounterModel : CounterContract.Model {
+
+    private var count = ZERO
+    override fun getCount(): String {
+        return count.toString()
+    }
+
+    override fun setInitialValue(value: Int) {
+        count = value
+    }
+
+    override fun increaseCount(value: Int) {
+        count += value
+    }
+
+    override fun decrementCount(value: Int) {
+        count -= value
+    }
+
+    override fun resetCount() {
+        count = ZERO
+    }
 }
